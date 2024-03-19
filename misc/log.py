a67bb2090a875e32e16afdb89904a9b351bb652e
faraday62 <faraday62@mail.ru>
2024-03-19 18:31:07 +0300

    disable aiokafka logger

--- misc/log.py
+++ misc/log.py
@@ -5,6 +5,11 @@ import os
 logger = logging.getLogger(__name__)
 LOG_ENV_KEY = 'SRVC_LOG'
 
+kafka_logger = logging.getLogger('aiokafka')
+kafka_logger.setLevel(logging.CRITICAL)
+for handler in kafka_logger.handlers:
+    handler.setLevel(kafka_logger.level)
+
 try:
     log_path = os.environ[LOG_ENV_KEY]
     if os.path.exists(log_path):
