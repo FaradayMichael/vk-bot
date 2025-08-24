7785ea5e952dfba78ca5e5096bf8290a1888eb11
m.istomov <m.istomov@notamedia.com.ru>
2025-08-24 18:59:52 +0300

    fix tags limit discord

--- app/schemas/images.py
+++ app/schemas/images.py
@@ -12,21 +12,20 @@ class ImageTags(BaseModel):
     description: str | None = None
     products_data: list[str] = []
 
-    def text(self, sep: str = '\n') -> str:
+    def text(self, sep: str = '\n', limit: int = None) -> str:
         str_lst = [f"tags: {self.tags_text}"]
         if self.description:
             str_lst.append(self.description)
         if self.products_data:
-            str_lst.append(self.products_data_text)
+            str_lst.append(self.get_products_data_text(limit))
         return sep.join(str_lst)
 
     @property
     def tags_text(self) -> str:
         return ', '.join(self.tags)
 
-    @property
-    def products_data_text(self) -> str:
-        return '\n'.join(self.products_data)
+    def get_products_data_text(self, limit: int = None) -> str:
+        return '\n'.join(self.products_data[:limit])
 
 
 class ImageTagsResponse(SuccessResponse):
